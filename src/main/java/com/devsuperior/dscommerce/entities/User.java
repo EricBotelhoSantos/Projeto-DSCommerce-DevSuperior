84bfa8d56a2dd95fc58c6f9954c365ce31956442
Eric Botelho Santos <dev.ericbotelho@gmail.com>
2026-09-13 14:57:12 -0300

    Order
    Criação das classes Order e OrderStatus

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.persistence.metamodel.IdentifiableType;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +19,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -76,5 +81,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
